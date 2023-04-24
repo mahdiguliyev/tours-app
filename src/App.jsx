66c0befe50df126a983a1fe7f01799a5c52d219f
Mahdi Guliyev <quliyev.mehdi.1997@gmail.com>
2023-04-24 15:45:18 +0400

    fetch the tours data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useState } from "react";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
